8b6fad50e45aa56146fce492f494e83e4ad6c00e
NikolasMelui <slak@samaradom.ru>
2018-06-03 20:50:30 +0400

    Add some new functional.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-import http from 'http';
-
-const server = http.createServer((req, res) => res.end('Welcome to multikey-node-boilerplate!'));
-
-server.listen(3000, () => global.console.log(`Server is listening on port 3000`));
+module.exports = async (req, res) => 'Its working';
