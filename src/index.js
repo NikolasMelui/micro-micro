87021a401ef7985a2ffdeb1fcd04c8921ab561e6
NikolasMelui <slak@samaradom.ru>
2018-06-03 22:46:57 +0400

    Add new code and deploy it into 'now'.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-module.exports = async (req, res) => 'Its working';
+const getRandomNumber = (min, max) => {
+	return Math.floor(min + Math.random() * (max + 1 - min));
+};
+
+module.exports = async (req, res) => {
+	return { randomNumber: getRandomNumber(1, 100) };
+};
