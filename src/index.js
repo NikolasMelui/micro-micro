f315ede058b8ce6a1d6d5fa04f68674e76201b65
NikolasMelui <slak@samaradom.ru>
2018-06-06 10:25:25 +0400

    Add all logic for nodejs restapi and graphql aggregator.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
-const getRandomNumber = (min, max) => {
-	return Math.floor(min + Math.random() * (max + 1 - min));
+const { send } = require('micro');
+const { get, post, router } = require('microrouter');
+const { makeExecutableSchema } = require('graphql-tools');
+const { microGraphql, microGraphiql } = require('apollo-server-micro');
+
+const typeDefs = `type Query {
+    message: String
+  }`;
+
+const resolvers = {
+	Query: {
+		message: () => 'First resolver',
+	},
 };
 
-module.exports = async (req, res) => {
-	return { randomNumber: getRandomNumber(1, 100) };
-};
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+const graphqlHandler = microGraphql({
+	schema,
+});
+const graphiqlHandler = microGraphiql({ endpointURL: '/graphql' });
+
+// const getRandomNumber = (min, max) => {
+// 	return Math.floor(min + Math.random() * (max + 1 - min));
+// };
+
+module.exports = router(
+	get('/graphql', graphqlHandler),
+	post('/graphql', graphqlHandler),
+	get('/graphiql', graphiqlHandler),
+	(req, res) => send(res, 404, 'There is no spoon')
+);
